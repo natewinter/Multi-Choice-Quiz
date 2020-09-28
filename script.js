7f27af5966d82fac9525a818bca22c306164f702
Josh Brown <joshbrown2794@gmail.com>
2020-09-27 21:49:57 -0700

    updating homework

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-// var questions = [ 
-//     {prompt: "what color are apples? \n (a) red/green \n\ (b) Purple\n (c) orange", answer : "a"},
-//     {prompt: "what color are bananas? \n (a) red/green \n\ (b) Yellow\n (c) orange", answer : "b"},
-//     {prompt: "what color are watermelons? \n (a) green \n\ (b) Yellow\n (c) orange", answer : "a"},
-// ]
+$(document).ready(function(){
+    //Hide all questions
+    $('.questionForm').hide();
+    
+    //show first question
+    $('#q1').show();
 
-// var questions = "";
-// var score = 0;
-
-// for(var i=0; i<questions.length; i++){
-//     var response = window.prompt(questions[i].prompt);
-// if(response == questions[i].answer){
-//     score++;
-//     alert("correct!");
-// } else{
-//     alert("wrong!")
-// }
-// }
-// alert("you got " + score + "/" + questions.length)
-var questionsEl = document.querySelector("#questions")
-var timer;
-var timeEl = document.querySelector("#time")
-var time = 120;
-
-function startQuiz(){
-    document.querySelector("#startPage").setAttribute("class", "hide")
-    questionsEl.classList.remove("hide")
-    timer = setInterval(timerCountdown,1000)
-    timeEl.textContent=time
-    getQuestion()
-}
-
-function timerCountdown(){
-    time--
-timeEl.textContent=time
-if (time<=0){
-    clearInterval(timer)
-    gameover()
-}
-
-}
-function getQuestion(){
-
-}
-
-function gameover(){
-console.log(gameover)
-}
-
-// document.querySelector("#startBtn").addEventListener("click",startQuiz)
-document.querySelector("#startBtn").onclick=startQuiz
+    $('#q1 #submit').click(function(){
+        $('.questionForm').hide();
+        $("#q2").fadeIn(300);
+        return false;        
+    });
+    $('#q2 #submit').click(function(){
+        $('.questionForm').hide();
+        $("#q3").fadeIn(300);
+        return false;        
+    });
+    $('#q3 #submit').click(function(){
+        $('.questionForm').hide();
+        $("#q4").fadeIn(300);
+        return false;        
+    });
+    $('#q4 #submit').click(function(){
+        $('.questionForm').hide();
+        $("#q5").fadeIn(300);
+        return false;        
+    });
+    $('#q5 #submit').click(function(){
+        $('.questionForm').hide();
+        $("#results").fadeIn(300);
+        return false;        
+    });
+});
